c66afddc67e42a1957b4924bdfc1772541522a20
Ryan McCartan <ryanjmccartan@gmail.com>
2019-10-05 17:55:23 -0500

    Get request is working

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class List extends Component {
+
+state = {
+    movies: []
+}
 
 componentDidMount() {
     this.getMovies();
@@ -8,12 +13,21 @@ componentDidMount() {
 
 getMovies = () => {
     console.log('in getMovies');
+    axios.get('/movies').then((response) => {
+        this.setState({movies: response.data})
+        console.log(this.state.movies);
+    }).catch((error) => {
+        console.log("Error with GET", error);
+    })
 }
 
 
     render() {
         return(
+            <>
             <p>List Page</p>
+            <p>{JSON.stringify(this.state.movies)}</p>
+            </>
         )
     }
 }
