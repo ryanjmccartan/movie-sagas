d92100f3e84c6d734d26bc9154f2e44f07ea3ae9
Ryan McCartan <ryanjmccartan@gmail.com>
2019-10-05 20:44:42 -0500

    Created route so user is able to click movie poster and get brought to details page, added button on details page that brings user back to movie list

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,9 +1,14 @@
 import React, {Component} from 'react';
+import {HashRouter as Router, Link} from 'react-router-dom';
 
 class Details extends Component {
     render() {
         return(
-            <p>Details Page</p>
+            <Router>
+                <p>Details Page</p>
+                <Link to="/"><button>Back to List</button></Link>
+                <button>Edit</button>
+            </Router>
         )
     }
 }
--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from "react-redux";
+import {HashRouter as Router, Link} from 'react-router-dom';
+
 
 class List extends Component {
     
@@ -14,11 +16,11 @@ getMovies = () => {
 
     render() {
         return(
-            <>
-            <p>{this.props.movieItem.title}</p>
-            <img src={this.props.movieItem.poster}/>
-            <p>{this.props.movieItem.description}</p>
-            </>
+            <Router>
+                <p>{this.props.movieItem.title}</p>
+                <Link to="/details"><button><img src={this.props.movieItem.poster}/></button></Link>
+                <p>{this.props.movieItem.description}</p>
+            </Router>
         )
     }
 }
